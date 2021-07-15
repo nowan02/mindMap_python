c4c36767dcb86773d2b63711a432203c8d218049
no_wan <56969728+nowan02@users.noreply.github.com>
2021-07-15 10:20:13 +0200

    Create mindMap.py
    Class and canvas defined, canvas is scrollable

---
+++ mindMap.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+mainWindow = Tk()
+mainWindow.geometry("1280x720")
+
+canvasFrame = Frame(width=1000, height=800)
+canvasFrame.pack(expand=True,fill=BOTH)
+canvas = Canvas(canvasFrame, bg="lightgrey", confine=False, width=8196, height=8196, scrollregion=(0,0,8196,8196))
+hbar = Scrollbar(canvasFrame, orient=HORIZONTAL)
+vbar = Scrollbar(canvasFrame, orient=VERTICAL)
+hbar.pack(side=BOTTOM, fill=X)
+vbar.pack(side=RIGHT, fill=Y)
+hbar.config(command=canvas.xview)
+vbar.config(command=canvas.yview)
+canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+
+
+
+
+
+class mindNode:
+    def __init__(self, x, y, height, width, color, textColor, text):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.color = color
+        self.textColor = textColor
+        self.text = text
+        self.children = list[mindNode]
+
+root = mindNode(0,0,200,100,"red","white","I am root")
+
+canvas.create_rectangle(root.x,root.y,root.x+root.width,root.y+root.height,fill=root.color)
+
+canvas.pack(expand=True, fill=BOTH)
+mainWindow.mainloop()
