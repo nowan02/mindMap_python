8eb39c38dfe4efc1a70739ea0eec4e675d90717c
nowan02 <starkgeri@gmail.com>
2021-07-21 16:48:44 +0200

    Added delete function, which deletes the node and it's children

--- mindMap.py
+++ mindMap.py
@@ -29,26 +29,42 @@ def nodeSelect(event):
     nodeId = event.widget.find_withtag('current')[0]
     print("mindNode with id:", nodeId, "clicked")
     search(root, nodeId)
-    print("mindNode with id:", selected, "selected")
+    print("mindNode with id:", selected.nodeId, "selected")
 
 class mindNode:
-    def __init__(self, x, y, text, textColor, rectColor):
+    def __init__(self, x, y, text, textColor, rectColor, parent):
 
         width = 200
         height = 100
 
-        if(len(text) > 200):
+        if(len(text) >= 200):
             height += 20 
 
+        self.deleted = False
         self.nodeId = canvas.create_rectangle(x, y, x+width, y+height, fill=rectColor)
         self.textId = canvas.create_text(x+width/2, y+height/2, font=("Arial",15), text=text, fill=textColor, width=width-10)
         canvas.tag_bind(self.nodeId,"<Button>",nodeSelect)
         self.children = []
 
-root = mindNode(0, 0, "I am root", "white", "red")
+    def delete(self):
+        canvas.delete(self.textId, self.nodeId)
+        self.deleted = True
+        for n in self.children:
+            n.delete()
+
+        def recurse(node:mindNode):
+            if(node.deleted == True):
+                node.parent.children.remove(self)
+            for n in self.children:
+                recurse(n)
+
+        recurse(self)
+
+root = mindNode(0, 0, "I am root", "white", "red", 1)
+print(root.nodeId)
 selected = root
 
-def wrapper(event):
+def editorWindow(x,y):
     editor = Toplevel(mainWindow)
     editor.title("Node editor")
     editor.geometry("300x300")
@@ -73,13 +89,18 @@ def wrapper(event):
 
     def confirm():
         if(tisColorEntry.get() != "" and tisTextColorEntry.get() != "" and tisTextEntry.get() != ""):
-            selected.children.append(mindNode(event.x, event.y, tisTextEntry.get(), tisTextColorEntry.get(), tisColorEntry.get()))
+            selected.children.append(mindNode(x, y, tisTextEntry.get(), tisTextColorEntry.get(), tisColorEntry.get(), selected))
             editor.destroy()
         else:
             print("No entry")
 
     confirmButton = Button(editor, text="Confirm", command=confirm)
     confirmButton.pack()
+
+def creationWrapper(event):
+    editorWindow(event.x,event.y)
+
+canvas.bind("<Shift-Button>", creationWrapper)
 
 canvas.pack()
 mainWindow.mainloop()
