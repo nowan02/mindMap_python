a89824d5f9a13efb2c71f1cbd6ed62b8134bf4e2
nowan02 <starkgeri@gmail.com>
2021-07-21 14:39:38 +0200

    node creation integrated into the class

--- mindMap.py
+++ mindMap.py
@@ -16,14 +16,6 @@ hbar.config(command=canvas.xview)
 vbar.config(command=canvas.yview)
 canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
 
-class mindNode:
-    def __init__(self, x, y, node, text):
-        self.x = x
-        self.y = y
-        self.nodeId = node
-        self.textId = text
-        self.children = []
-
 def search(parentNode, searchId):
     global selected
     if(parentNode.nodeId == searchId):
@@ -36,38 +28,30 @@ def search(parentNode, searchId):
 def nodeSelect(event):
     nodeId = event.widget.find_withtag('current')[0]
     print("mindNode with id:", nodeId, "clicked")
-    search(rootNode, nodeId)
-    print(selected.nodeId)
+    search(root, nodeId)
+    print("mindNode with id:", selected, "selected")
 
-def addNode(x, y, width, height, text, color, textColor):
-    rectangle = canvas.create_rectangle(x, y, x+width, y+height, fill=color)
-    canvas.tag_bind(rectangle,"<Button>",nodeSelect)
-    text = canvas.create_text(x+width/2, y+width/2, font=("Arial",15), text=text, fill=textColor, width=width)
-    newNode = mindNode(x, y, rectangle,text)
-    selected.children.append(newNode)
+class mindNode:
+    def __init__(self, x, y, text, textColor, rectColor):
 
-root = canvas.create_rectangle(0,0,200,100,fill="red")
-rootText = canvas.create_text(100,50,font=("Arial",15),fill="white",width=100,text="I am root")
-rootNode = mindNode(0, 0, root, rootText)
-selected = rootNode
-canvas.tag_bind(root,"<Button>",nodeSelect) 
+        width = 200
+        height = 100
 
-def clickWrapper(event):
+        if(len(text) > 200):
+            height += 20 
+
+        self.nodeId = canvas.create_rectangle(x, y, x+width, y+height, fill=rectColor)
+        self.textId = canvas.create_text(x+width/2, y+height/2, font=("Arial",15), text=text, fill=textColor, width=width-10)
+        canvas.tag_bind(self.nodeId,"<Button>",nodeSelect)
+        self.children = []
+
+root = mindNode(0, 0, "I am root", "white", "red")
+selected = root
+
+def wrapper(event):
     editor = Toplevel(mainWindow)
     editor.title("Node editor")
     editor.geometry("300x300")
-
-    tisWidth = Label(editor, text="Width")
-    tisWidth.config(font=('Arial', 14))
-    tisWidth.pack()
-    tisWidthEntry = Entry(editor)
-    tisWidthEntry.pack()
-
-    tisHeight = Label(editor, text="Height")
-    tisHeight.config(font=('Arial', 14))
-    tisHeight.pack()
-    tisHeightEntry = Entry(editor)
-    tisHeightEntry.pack()
 
     tisColor = Label(editor, text="Color")
     tisColor.config(font=('Arial', 14))
@@ -88,8 +72,8 @@ def clickWrapper(event):
     tisTextEntry.pack()
 
     def confirm():
-        if(tisWidthEntry.get() != "" and tisHeightEntry.get() != "" and tisColorEntry.get() != "" and tisTextColorEntry.get() != "" and tisTextEntry.get() != ""):
-            addNode(event.x, event.y, int(tisWidthEntry.get()), int(tisHeightEntry.get()), tisTextEntry.get(), tisColorEntry.get(), tisTextColorEntry.get())
+        if(tisColorEntry.get() != "" and tisTextColorEntry.get() != "" and tisTextEntry.get() != ""):
+            selected.children.append(mindNode(event.x, event.y, tisTextEntry.get(), tisTextColorEntry.get(), tisColorEntry.get()))
             editor.destroy()
         else:
             print("No entry")
@@ -97,7 +81,5 @@ def clickWrapper(event):
     confirmButton = Button(editor, text="Confirm", command=confirm)
     confirmButton.pack()
 
-canvas.bind("<Shift-Button>", clickWrapper)
-
-canvas.pack(expand=True, fill=BOTH)
+canvas.pack()
 mainWindow.mainloop()
